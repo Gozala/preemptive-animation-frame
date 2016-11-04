45dd8984b91eada4c6ffd6eabd70598d45ffb76f
Irakli Gozalishvili <rfobic@gmail.com>
2016-11-03 17:22:34 -0700

    Update tests.

--- test/api.js
+++ test/api.js
@@ -1,10 +1,16 @@
-"use strict"
+'use strict'
 
 var PAF = require('../')
 var test = require('tape')
 
-test('api: lintFiles', function (assert) {
-  assert.equal(typeof(PAF.requestAnimationFrame), 'function')
-  assert.equal(typeof(PAF.cancelAnimationFrame), 'function')
+test('api check', function (assert) {
+  assert.ok(isFunction(PAF.requestAnimationFrame), 'exports requestAnimationFrame')
+  assert.ok(isFunction(PAF.cancelAnimationFrame), 'exports cancelAnimationFrame')
+  assert.ok(isFunction(PAF.forceAnimationFrame), 'exports forceAnimationFrame')
+  assert.ok(isFunction(PAF.now), 'exports now function')
   assert.end()
 })
+
+const isFunction =
+  value =>
+  typeof value === 'function'
